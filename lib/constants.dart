6fde2945d61dddcfa4303fcb8345497b92403fd5
Safa <safaanwar2211@gmail.com>
2023-03-31 03:20:17 -0700

    initial commit

---
+++ lib/constants.dart
@@ -0,0 +1,10 @@
+import 'home_screen.dart';
+import 'package:flutter/material.dart';
+
+const Color whiteColor=Colors.white;
+const Color grayColor = Colors.grey;
+const Color purpleColor = Colors.purple;
+ const headingTextStyle = TextStyle(
+   fontSize: 10,
+   color: whiteColor,
+   fontWeight: FontWeight.w900);
